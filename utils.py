2dbc50e31c49822f3ea6b9c01cc7391069a9122d
SophiaYu <yurong_2006_1991@163.com>
2018-11-26 16:32:40 +0800

    为获取数据接口添加必要保护。

--- utils.py
+++ utils.py
@@ -38,6 +38,7 @@ def get_train_data(vocabulary, batch_size, num_steps):
     """
     vocabulary = np.array(copy.copy(vocabulary))
     vocabulary = vocabulary.flatten()
+    vocabulary = vocabulary[: 3000]
     # batch数量
     n_batches = int(len(vocabulary) / (batch_size*num_steps))
 
@@ -45,7 +46,6 @@ def get_train_data(vocabulary, batch_size, num_steps):
 
     # 变为batch_size行，num_steps*n_batches列
     vocabulary = vocabulary.reshape((batch_size, -1))
-
 
     # 为vocabulary每行最后添加下一个元素（用于获取lable）
     temp = vocabulary[:, 0:1]
@@ -55,7 +55,11 @@ def get_train_data(vocabulary, batch_size, num_steps):
     while True:
         # 打乱vocabulary行
         np.random.shuffle(vocabulary)
+        is_over = False
         for n in range(0, vocabulary.shape[1], num_steps):
+            if n + num_steps + 1 >= vocabulary.shape[1]:
+                is_over = True
+                break
             input = vocabulary[:, n:n+num_steps]
             # label的0到倒数第二列与input的1到最后一列相同，
             # 例：输入为 “老夫聊发少年”，则对应的label为"夫聊发少年狂"
@@ -64,6 +68,9 @@ def get_train_data(vocabulary, batch_size, num_steps):
             # 把一个函数变成一个 generator,调用函数不会执行函数，而是返回一个 iterable 对象
             # 下次迭代时，代码从 yield 的下一条语句继续执行
             yield input, lable
+        if is_over:
+            print('Have trained all of the data once!')
+            break
 # def get_train_data(vocabulary, batch_size, num_steps):
 #     """
 #     获取数据
