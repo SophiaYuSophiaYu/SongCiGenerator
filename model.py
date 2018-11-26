37f695e6ce7eebef8fc79a22b22cbf6c719a3a27
SophiaYu <yurong_2006_1991@163.com>
2018-11-26 16:00:47 +0800

    code commit

---
+++ model.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import tensorflow as tf
+
+
+class Model():
+    def __init__(self, learning_rate=0.001, batch_size=16, num_steps=32, num_words=5000, dim_embedding=128, rnn_layers=3):
+        r"""初始化函数
+
+        Parameters
+        ----------
+        learning_rate : float
+            学习率.
+        batch_size : int
+            batch_size,一个batch有多少输入序列.
+        num_steps : int
+            RNN有多少个time step，也就是输入数据每个seq的长度.
+        num_words : int
+            字典里有多少个字，用作embeding变量的第一个维度的确定和onehot编码.
+        dim_embedding : int
+            embding中，编码后的字向量的维度
+        rnn_layers : int
+            有多少个RNN层，在这个模型里，一个RNN层就是一个RNN Cell，各个Cell之间通过TensorFlow提供的多层RNNAPI（MultiRNNCell等）组织到一起
+
+        batch大小 = batch_size * num_steps
+
+        """
+        self.batch_size = batch_size
+        self.num_steps = num_steps
+        self.num_words = num_words
+        self.dim_embedding = dim_embedding
+        self.rnn_layers = rnn_layers
+        self.learning_rate = learning_rate
+
+    def build(self, embedding_file=None):
+
+        # 创建一个LSTM Cell
+        def get_a_cell(state_size=128, keep_prob=1):
+            # LSTM Cell
+            lstm = tf.nn.rnn_cell.BasicLSTMCell(num_units=state_size)
+            # Dropout防止过拟合
+            drop = tf.nn.rnn_cell.DropoutWrapper(cell=lstm, output_keep_prob=keep_prob)
+            return drop
+
+
+        # global step
+        self.global_step = tf.Variable(
+            0, trainable=False, name='self.global_step', dtype=tf.int64)
+
+        self.X = tf.placeholder(
+            tf.int32, shape=[self.batch_size, self.num_steps], name='input')
+        self.Y = tf.placeholder(
+            tf.int32, shape=[self.batch_size, self.num_steps], name='label')
+
+        self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+
+        with tf.variable_scope('embedding'):
+            if embedding_file:
+                # if embedding file provided, use it.
+                embedding = np.load(embedding_file)
+                embed = tf.constant(embedding, name='embedding')
+            else:
+                # if not, initialize an embedding and train it.
+                embed = tf.get_variable(
+                    'embedding', [self.num_words, self.dim_embedding])
+                tf.summary.histogram('embed', embed)
+
+            self.rnn_inputs = tf.nn.embedding_lookup(embed, self.X)
+
+        with tf.variable_scope('rnn'):
+            # 用tf.nn.rnn_cell MultiRNNCell创建self.rnn_layers层RNN
+            # state_size是(self.dim_embedding,self.dim_embedding,self.dim_embedding)也就是每个隐层大小均为128
+            cell = tf.nn.rnn_cell.MultiRNNCell(
+                [get_a_cell(self.dim_embedding, self.keep_prob) for _ in range(self.rnn_layers)])
+
+            # 初始状态，通过zero_state得到一个全0的初始状态
+            self.initial_state = cell.zero_state(self.batch_size, tf.float32)
+
+            # 通过dynamic_rnn对cell在时间维度进行展开 lstm_outputs 64*32*128
+            self.lstm_outputs, self.final_state = tf.nn.dynamic_rnn(cell, self.rnn_inputs,
+                                                                    initial_state=self.initial_state)
+
+            # 通过lstm_outputs得到概率
+            seq_output = tf.concat(self.lstm_outputs, 1)
+
+            # flatten it  展开
+            seq_output_final = tf.reshape(seq_output, [-1, self.dim_embedding])
+
+
+        with tf.variable_scope('softmax'):
+            softmax_w = tf.Variable(tf.truncated_normal([self.dim_embedding, self.num_words], stddev=0.1))
+            softmax_b = tf.Variable(tf.zeros(self.num_words))
+
+        self.logits = tf.matmul(seq_output_final, softmax_w) + softmax_b
+        tf.summary.histogram('logits', self.logits)
+
+        self.predictions = tf.nn.softmax(self.logits, name='predictions')
+
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.logits, labels=tf.reshape(self.Y, [-1]))
+        mean, var = tf.nn.moments(self.logits, -1)
+        self.loss = tf.reduce_mean(loss)
+        tf.summary.scalar('logits_loss', self.loss)
+
+        var_loss = tf.divide(10.0, 1.0+tf.reduce_mean(var))
+        tf.summary.scalar('var_loss', var_loss)
+        # 把标准差作为loss添加到最终的loss里面，避免网络每次输出的语句都是机械的重复
+        self.loss = self.loss + var_loss
+        tf.summary.scalar('total_loss', self.loss)
+
+        # gradient clip
+        tvars = tf.trainable_variables()
+        grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars), 5)
+        train_op = tf.train.AdamOptimizer(self.learning_rate)
+        self.optimizer = train_op.apply_gradients(
+            zip(grads, tvars), global_step=self.global_step)
+
+        tf.summary.scalar('loss', self.loss)
+
+        self.merged_summary_op = tf.summary.merge_all()
---
+++ utils.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import collections
+import copy
+
+import numpy as np
+
+
+def read_data(filename):
+    with open(filename, encoding="utf-8") as f:
+        data = f.read()
+    data = list(data)
+    return data
+
+
+def index_data(sentences, dictionary):
+    shape = sentences.shape
+    sentences = sentences.reshape([-1])
+    index = np.zeros_like(sentences, dtype=np.int32)
+    for i in range(len(sentences)):
+        try:
+            index[i] = dictionary[sentences[i]]
+        except KeyError:
+            index[i] = dictionary['UNK']
+
+    return index.reshape(shape)
+
+# 数据处理中，data为文本中一段随机截取的文字，label为data对应的下一个标号的文字。
+# 以苏轼的江神子（江城子）为例：输入为 “老夫聊发少年”，则对应的label为"夫聊发少年狂"
+def get_train_data(vocabulary, batch_size, num_steps):
+    """
+    获取数据
+    :param vocabulary: word列表
+    :param batch_size: batch大小
+    :param num_steps: time step 数量，即每个seq长度
+    :return:
+    """
+    vocabulary = np.array(copy.copy(vocabulary))
+    vocabulary = vocabulary.flatten()
+    # batch数量
+    n_batches = int(len(vocabulary) / (batch_size*num_steps))
+
+    vocabulary = vocabulary[: batch_size*num_steps*n_batches]
+
+    # 变为batch_size行，num_steps*n_batches列
+    vocabulary = vocabulary.reshape((batch_size, -1))
+
+
+    # 为vocabulary每行最后添加下一个元素（用于获取lable）
+    temp = vocabulary[:, 0:1]
+    temp[:-1, :] = temp[1:, :]
+    vocabulary = np.concatenate((vocabulary, temp), axis=1)
+
+    while True:
+        # 打乱vocabulary行
+        np.random.shuffle(vocabulary)
+        for n in range(0, vocabulary.shape[1], num_steps):
+            input = vocabulary[:, n:n+num_steps]
+            # label的0到倒数第二列与input的1到最后一列相同，
+            # 例：输入为 “老夫聊发少年”，则对应的label为"夫聊发少年狂"
+            lable = vocabulary[:, n+1:n+num_steps+1]
+
+            # 把一个函数变成一个 generator,调用函数不会执行函数，而是返回一个 iterable 对象
+            # 下次迭代时，代码从 yield 的下一条语句继续执行
+            yield input, lable
+# def get_train_data(vocabulary, batch_size, num_steps):
+#     """
+#     获取数据
+#     :param vocabulary: word列表
+#     :param batch_size: 一个batch有多少seq序列
+#     :param num_steps: time step 数量，即每个seq长度
+#     :yield: input 输入数据  lable 目标数据
+#     """
+#
+#     vocabulary = np.array(copy.copy(vocabulary))
+#
+#     vocabulary = vocabulary.reshape((1, -1))
+#
+#     # # 为vocabulary每行最后添加下一个元素（用于获取lable）
+#     # temp = vocabulary[:, 0:1]
+#     # temp[:-1, :] = temp[1:, :]
+#     # vocabulary = np.concatenate((vocabulary, temp), axis=1)
+#     print("vocabulary shape", vocabulary.shape)
+#
+#     while True:
+#         # 打乱vocabulary行
+#         # np.random.shuffle(vocabulary)
+#         is_over = False
+#         for n in range(0, vocabulary.shape[1], num_steps):
+#             print('n:', n)
+#             if n+num_steps+1 >=vocabulary.shape[1]:
+#                 is_over = True
+#                 break
+#             input = vocabulary[:, n : n+num_steps]
+#
+#             # label的0到倒数第二列与input的1到最后一列相同，
+#             # 例：输入为 “老夫聊发少年”，则对应的label为"夫聊发少年狂"
+#             lable = vocabulary[:, n+1 : n+num_steps+1]
+#
+#             # 把一个函数变成一个 generator,调用函数不会执行函数，而是返回一个 iterable 对象
+#             # 下次迭代时，代码从 yield 的下一条语句继续执行
+#             yield input, lable
+#
+#         if is_over:
+#             print('over~~~~~~~~~~~~~~~~')
+#             break
+
+
+def build_dataset(words, n_words):
+    """Process raw inputs into a dataset."""
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(n_words - 1))
+    dictionary = dict()
+    for word, _ in count:
+        dictionary[word] = len(dictionary)
+    data = list()
+    unk_count = 0
+    for word in words:
+        index = dictionary.get(word, 0)
+        if index == 0:  # dictionary['UNK']
+            unk_count += 1
+        data.append(index)
+    count[0][1] = unk_count
+    reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reversed_dictionary
